85201ac130902e410967a4b31710ed5d89d55b48
RajuAhmed2 <92273686+RajuAhmed2@users.noreply.github.com>
2022-11-25 15:45:59 +0600

    first commit

---
+++ assets/js/active.js
@@ -0,0 +1,140 @@
+/*
+Template: Techex - Information & Technology HTML Template
+Author: RRDevs
+*/
+
+(function($) {
+    "use strict";
+
+
+    $(document).ready( function() {
+
+        //# Smooth Scroll
+        $('#responsive-menu a').on('click', function(event) {
+            var $anchor = $(this);
+            var headerH = '85';
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - headerH + "px"
+            }, 1000, 'easeInOutExpo');
+            event.preventDefault();
+        });
+
+        // Sticky Menu
+        $(window).scroll(function() {
+            var Width = $(document).width();
+
+            if ($("body").scrollTop() > 100 || $("html").scrollTop() > 100) {
+                if (Width > 767) {
+                    $("header").addClass("sticky");
+                }
+            } else {
+                $("header").removeClass("sticky");
+            }
+        });
+
+        $('.container').imagesLoaded(function() {
+            $('.portfolio-cat-filter').on('click', 'button', function() {
+                var filterValue = $(this).attr('data-filter');
+                $grid.isotope({ filter: filterValue });
+            });
+
+            $('.case-cat-filter').on('click', 'button', function() {
+                var filterValue = $(this).attr('data-filter');
+                $grid.isotope({ filter: filterValue });
+            });
+
+        });
+
+        var catButton = '.portfolio-cat-filter button';
+
+        var caseButton = '.case-cat-filter button';
+
+        $(catButton).on('click', function(){
+            $(catButton).removeClass('active');
+            $(this).addClass('active');
+        });
+
+        $(caseButton).on('click', function(){
+            $(caseButton).removeClass('active');
+            $(this).addClass('active');
+        });
+
+        $('#hamburger').on('click', function() {            
+            $('.mobile-nav').addClass('show');
+            $('.overlay').addClass('active');
+        });
+
+        $('.close-nav').on('click', function() {            
+            $('.mobile-nav').removeClass('show');            
+            $('.overlay').removeClass('active');          
+        });
+
+        $(".overlay").on("click", function () {
+            $(".mobile-nav").removeClass("show");
+            $('.overlay').removeClass('active');
+        });
+
+        $("#mobile-menu").metisMenu();
+
+        new WOW().init();
+
+
+        $('.side-toggle-menu, .offcanvas-btn').on('click', function() {            
+            $('.offset-menu').addClass('show');
+        });
+
+        $('#offset-menu-close-btn').on('click', function() {            
+            $('.offset-menu').removeClass('show');
+        });
+
+
+
+    }); // end document ready function
+
+    function loader() {
+        $(window).on('load', function() {
+            // Animate loader off screen
+            $(".preloader").delay(500).fadeOut();                        
+        });
+    }
+
+    loader();
+
+    //TIMES 
+        
+    (function () {
+        const second = 1000,
+            minute = second * 60,
+            hour = minute * 60,
+            day = hour * 24;
+    
+        //I'm adding this section so I don't have to keep updating this pen every year :-)
+        //remove this if you don't need it
+        let today = new Date(),
+            dd = String(today.getDate()).padStart(2, "0"),
+            mm = String(today.getMonth() + 1).padStart(2, "0"),
+            yyyy = today.getFullYear(),
+            nextYear = yyyy + 1,
+            dayMonth = "12/20/",
+            birthday = dayMonth + yyyy;
+        
+        today = mm + "/" + dd + "/" + yyyy;
+        //end
+        
+        const countDown = new Date(birthday).getTime(),
+            x = setInterval(function() {    
+    
+            const now = new Date().getTime(),
+                    distance = countDown - now;
+    
+            document.getElementById("day").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("munite").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("secend").innerText = Math.floor((distance % (minute)) / second);
+    
+            //seconds
+            }, 0)
+        }());
+
+
+})(jQuery); // End jQuery
